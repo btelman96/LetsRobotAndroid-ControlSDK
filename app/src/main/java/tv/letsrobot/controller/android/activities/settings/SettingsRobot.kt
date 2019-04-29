8fb1207492efbf430a70ccdd84a1cb89b101f058
Brendon Telman <b.telman01@gmail.com>
2019-04-28 20:40:55 -0500

    Hide settings icon if specific robot component does not need it

--- app/src/main/java/tv/letsrobot/controller/android/activities/settings/SettingsRobot.kt
+++ app/src/main/java/tv/letsrobot/controller/android/activities/settings/SettingsRobot.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.preference.ListPreference
+import androidx.preference.Preference
 import tv.letsrobot.android.api.robot.CommunicationType
 import tv.letsrobot.android.api.utils.RobotConfig
 import tv.letsrobot.android.api.utils.getEntries
@@ -47,10 +48,31 @@ class SettingsRobot : BasePreferenceFragmentCompat(
         pref.entries = enumValue.getEntries()
         pref.entryValues = enumValue.getEntryValues()
         pref.value = enumValue.name
+        maybeDisplayExpandedSetup(pref, key)
         pref.setOnPreferenceChangeListener { preference, newValue ->
             val any = searchForEnum(newValue, enumValue)
             any?.let { key.saveValue(context!!, it) }
+            maybeDisplayExpandedSetup(preference, key)
             true
+        }
+    }
+
+    private fun maybeDisplayExpandedSetup(preference: Preference?, key: RobotConfig) {
+        val settingsPref = preference as? ListSettingsPreference
+        settingsPref?.let {
+            it.hideSecondTarget = !hasSetup(key)
+        }
+    }
+
+    private fun hasSetup(key : RobotConfig) : Boolean{
+        return when(key){
+            RobotConfig.Communication -> {
+                val enum = RobotConfig.Communication.getValue(context!!) as CommunicationType
+                val clazz = enum.getInstantiatedClass
+                return clazz?.needsSetup(activity!!) == true //because result could be null
+            }
+//            RobotConfig.Protocol -> TODO()
+            else -> false
         }
     }
 
--- app/src/main/java/tv/letsrobot/controller/android/ui/settings/ListSettingsPreference.kt
+++ app/src/main/java/tv/letsrobot/controller/android/ui/settings/ListSettingsPreference.kt
@@ -9,17 +9,22 @@ import tv.letsrobot.controller.android.R
 class ListSettingsPreference : TwoTargetListPreference {
 
     constructor(context: Context, attrs: AttributeSet,
-                defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
-    }
+                defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    constructor(context: Context) : super(context) {}
+    constructor(context: Context) : super(context)
+
+    var hideSecondTarget: Boolean = false
 
     override fun getSecondTargetResId(): Int {
         return R.layout.preference_widget_master_settings
+    }
+
+    override fun shouldHideSecondTarget(): Boolean {
+        return hideSecondTarget || super.shouldHideSecondTarget()
     }
 
     private var widgetView: View? = null
