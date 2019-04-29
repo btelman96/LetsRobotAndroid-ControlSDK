f39ea4be4f627606c6e8d8c08bbcc1a3711e807c
Brendon Telman <b.telman01@gmail.com>
2019-04-28 21:14:07 -0500

    Share behavior with custom image view. Extend androidx

--- app/src/main/java/tv/letsrobot/controller/android/ui/settings/CustomImageView.kt
+++ app/src/main/java/tv/letsrobot/controller/android/ui/settings/CustomImageView.kt
@@ -1,17 +1,14 @@
 package tv.letsrobot.controller.android.ui.settings
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
 
 /**
  * Image View that follows other UI behavior if disabled
  */
-@SuppressLint("AppCompatCustomView")
-class CustomImageView @JvmOverloads constructor(
+open class CustomImageView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr) {
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
         imageAlpha = if(enabled) 0xFF else 0x3F
